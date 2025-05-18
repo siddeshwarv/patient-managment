016e35501ff5d120981124755aa43c59f9706e7e
Vasam Siddeshwar <siddeshwar.vasam@gmail.com>
2025-05-18 11:17:41 +0530

    Added patient service

---
+++ patient-service/src/main/java/com/pm/patient_service/model/Patient.java
@@ -0,0 +1,36 @@
+package com.pm.patient_service.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.UUID;
+
+import javax.annotation.processing.Generated;
+import java.time.LocalDate;
+
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+}
